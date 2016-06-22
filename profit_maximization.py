8dae54662f34bf6d22b73f97c7da1baa28e0a37d
Theja Tulabandhula <theja.tulabandhula@xerox.com>
2016-06-22 21:26:36 +0530

    baseline looks okay

--- profit_maximization.py
+++ profit_maximization.py
@@ -1,21 +1,20 @@
-import random, time, pulp, matplotlib, math, copy
+import random, time, pulp, math, copy
 import networkx as nx
 import numpy as np
 from pprint import pprint
 from collections import OrderedDict
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+# import  matplotlib
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
 random.seed(1000)  # for replicability of experiments.
 __author__ = 'q4fj4lj9'
-
-
 
 
 def generate_instance():
 
 	#requests and service provider details
 
-	NO_OF_REQUESTS_IN_UNIVERSE = 20
+	NO_OF_REQUESTS_IN_UNIVERSE = 30
 	INITIAL_MARKET_SHARE_OF_SERVICE_PROVIDER = .6
 	INITIAL_RIDE_SHARING_AMONG_SERVICE_PROVIDER = .3
 	BETA1 = 0.9
@@ -68,20 +67,28 @@ def generate_instance():
 		else:
 			all_requests[i]['RIDE_SHARING'] = False
 
+
+	#permutations needed for two participant matching
+	all_permutations_two = {('si','sj','di','dj'):'case1',
+			('si','sj','dj','di'):'case2',
+			('sj','si','dj','di'):'case1',
+			('sj','si','di','dj'):'case2'}#TODO: make more comprehensible
+
 	return {'all_requests': all_requests,
-	'params':{
+	'instance_params':{
 	'OUR_CUT_FROM_DRIVER': OUR_CUT_FROM_DRIVER,
-	'ALPHA_OP':ALPHA_OP}}
+	'ALPHA_OP':ALPHA_OP,
+	'all_permutations_two': all_permutations_two}}
 
 def euclidean(x,y):
 	assert x is not None and y is not None
 	return np.linalg.norm(x - y)
 
-def get_driving_distance(selected_requests,permutation,all_permutations,instance):
+def get_driving_distance(focus_request,selected_requests,permutation,instance):
 
 	#local
-	all_requests = instance['all_requests'] #TODO
-	params = instance['params']
+	all_requests = instance['all_requests']
+	all_permutations_two = instance['instance_params']['all_permutations_two']
 
 	#Logic
 	if len(selected_requests) > 2:
@@ -93,29 +100,67 @@ def get_driving_distance(selected_requests,permutation,all_permutations,instance
 	if len(selected_requests) == 2:
 
 		i = selected_requests[0]
-		j = selected_requests[1]	
+		j = selected_requests[1]
+
+		#None case remains the same
+		if focus_request == i:
+			focus_request = 'i'
+		elif focus_request == j:
+			focus_request = 'j'
+
 		if permutation[0]=='sj':#i and j are symmetric within each case. See board photo. TODO
-			(i,j) = (j,i)
 
-		if all_permutations[permutation] == 'case1':
+			#None case remains the same
+			if focus_request == 'i':
+				focus_request = 'j'
+			elif focus_request == 'j':
+				focus_request = 'i'
 			
-			return euclidean(all_requests[i]['orig'],all_requests[j]['orig']) + \
-					euclidean(all_requests[j]['orig'],all_requests[i]['dest']) + \
-					euclidean(all_requests[i]['dest'],all_requests[j]['dest'])
+			(i,j) = (j,i) #swap
 
-		elif all_permutations[permutation] == 'case2':
-
-			return euclidean(all_requests[i]['orig'],all_requests[j]['orig']) + \
-					euclidean(all_requests[j]['orig'],all_requests[j]['dest']) + \
-					euclidean(all_requests[j]['dest'],all_requests[i]['dest'])
-		else:
-			print "Error!" #TODO
-			return -1 #TODO
-
-def check_SIR_satisfaction_general(selected_requests,permutation,instance,all_permutations):
 	
-	all_requests = instance['all_requests'] #TODO
-	params = instance['params']
+		if all_permutations_two[permutation] == 'case1':
+			
+			if focus_request == None:
+				return euclidean(all_requests[i]['orig'],all_requests[j]['orig']) + \
+						euclidean(all_requests[j]['orig'],all_requests[i]['dest']) + \
+						euclidean(all_requests[i]['dest'],all_requests[j]['dest'])
+
+			elif focus_request == 'i':
+				return euclidean(all_requests[i]['orig'],all_requests[j]['orig']) + \
+						euclidean(all_requests[j]['orig'],all_requests[i]['dest'])
+
+			elif focus_request == 'j':
+				return euclidean(all_requests[j]['orig'],all_requests[i]['dest']) + \
+						euclidean(all_requests[i]['dest'],all_requests[j]['dest'])
+			else:
+				raise Exception
+
+
+		elif all_permutations_two[permutation] == 'case2':
+
+			if focus_request == None or focus_request == 'i':
+
+				return euclidean(all_requests[i]['orig'],all_requests[j]['orig']) + \
+						euclidean(all_requests[j]['orig'],all_requests[j]['dest']) + \
+						euclidean(all_requests[j]['dest'],all_requests[i]['dest'])
+			elif focus_request == 'j':
+				return euclidean(all_requests[j]['orig'],all_requests[j]['dest'])
+			else:
+				raise Exception
+		else:
+			raise Exception
+	else:
+		raise Exception
+
+def check_SIR_satisfaction_general(selected_requests,permutation,instance,experiment_params):
+	
+	#if sir setting is false, then we should not check SIR
+	if experiment_params['SIR_SETTING']==False:
+		return True
+
+	all_requests = instance['all_requests'] #local copy
+	all_permutations_two = instance['instance_params']['all_permutations_two']
 
 	#Logic
 	if len(selected_requests) > 2:
@@ -132,23 +177,23 @@ def check_SIR_satisfaction_general(selected_requests,permutation,instance,all_pe
 		if permutation[0]=='sj':#i and j are symmetric within each case. See board photo. TODO
 			(i,j) = (j,i)		
 
-		if all_permutations[permutation] == 'case1':
+		if all_permutations_two[permutation] == 'case1':
 
 			constraint_i =  (all_requests[i]['our_cut_from_requester'][1] - all_requests[i]['our_cut_from_requester'][2])*euclidean(all_requests[i]['orig'],all_requests[i]['dest']) \
-				>= (params['GAMMA'] + all_requests[i]['detour_sensitivity_normalized'])*\
+				>= (experiment_params['GAMMA'] + all_requests[i]['detour_sensitivity_normalized'])*\
 					(euclidean(all_requests[i]['orig'],all_requests[j]['orig']) + \
 					euclidean(all_requests[j]['orig'],all_requests[i]['dest']) - \
 					euclidean(all_requests[i]['orig'],all_requests[i]['dest']))
 			constraint_j = (all_requests[j]['our_cut_from_requester'][1] - all_requests[j]['our_cut_from_requester'][2])*euclidean(all_requests[j]['orig'],all_requests[j]['dest']) \
-				>= (params['GAMMA'] + all_requests[j]['detour_sensitivity_normalized'])*\
+				>= (experiment_params['GAMMA'] + all_requests[j]['detour_sensitivity_normalized'])*\
 					(euclidean(all_requests[j]['orig'],all_requests[i]['dest']) + \
 					euclidean(all_requests[i]['dest'],all_requests[j]['dest']) - \
 					euclidean(all_requests[j]['orig'],all_requests[j]['dest']))
 
-		elif all_permutations[permutation] == 'case2':
+		elif all_permutations_two[permutation] == 'case2':
 
 			constraint_i = (all_requests[i]['our_cut_from_requester'][1] - all_requests[i]['our_cut_from_requester'][2])*euclidean(all_requests[i]['orig'],all_requests[i]['dest']) \
-				>= (params['GAMMA'] + all_requests[i]['detour_sensitivity_normalized'])*\
+				>= (experiment_params['GAMMA'] + all_requests[i]['detour_sensitivity_normalized'])*\
 					(euclidean(all_requests[i]['orig'],all_requests[j]['orig']) + \
 					euclidean(all_requests[j]['orig'],all_requests[j]['dest']) + \
 					euclidean(all_requests[j]['dest'],all_requests[i]['dest']) - \
@@ -162,10 +207,15 @@ def check_SIR_satisfaction_general(selected_requests,permutation,instance,all_pe
 
 		return constraint_i and constraint_j
 
-def check_SIR_satisfaction_detour_based(selected_requests,permutation,instance,all_permutations):
+def check_SIR_satisfaction_detour_based(selected_requests,permutation,instance,experiment_params):
 	
-	all_requests = instance['all_requests'] #TODO
-	params = instance['params']
+	#if sir setting is false, then we should not check SIR
+	if experiment_params['SIR_SETTING']==False:
+		return True
+
+	all_requests = instance['all_requests'] #local copy
+	all_permutations_two = instance['instance_params']['all_permutations_two']
+
 
 	#Logic
 	if len(selected_requests) > 2:
@@ -183,15 +233,15 @@ def check_SIR_satisfaction_detour_based(selected_requests,permutation,instance,a
 			(i,j) = (j,i)		
 
 		constraint_i = all_requests[i]['our_cut_from_requester'][1]\
-				>= params['GAMMA'] + all_requests[i]['detour_sensitivity_normalized']
+				>= experiment_params['GAMMA'] + all_requests[i]['detour_sensitivity_normalized']
 		constraint_j = all_requests[j]['our_cut_from_requester'][1]\
-				>= params['GAMMA'] + all_requests[j]['detour_sensitivity_normalized']
+				>= experiment_params['GAMMA'] + all_requests[j]['detour_sensitivity_normalized']
 
-		if all_permutations[permutation] == 'case1':
+		if all_permutations_two[permutation] == 'case1':
 
 			return constraint_i and constraint_j
 
-		elif all_permutations[permutation] == 'case2':
+		elif all_permutations_two[permutation] == 'case2':
 
 			return constraint_i
 
@@ -199,17 +249,69 @@ def check_SIR_satisfaction_detour_based(selected_requests,permutation,instance,a
 			print "Error!" #TODO
 			return False
 
-def get_profit(selected_requests,instance):
+def get_profit_non_ride_sharing(selected_request_as_list,instance):
+	assert instance is not None
+	assert selected_request_as_list is not None	
+	#single id is input as a list to maintain API consistency with get_profit() function
+
+	#print 'selected_request_as_list',selected_request_as_list
+
+	return instance['instance_params']['ALPHA_OP']*\
+		instance['instance_params']['OUR_CUT_FROM_DRIVER']*\
+				euclidean(instance['all_requests'][selected_request_as_list[0]]['orig'],instance['all_requests'][selected_request_as_list[0]]['dest'])
+
+def get_profit_unmatched(selected_request_as_list,instance):
+	assert instance is not None
+	assert selected_request_as_list is not None	
+	#single id is input as a list to maintain API consistency with get_profit() function
+
+	return instance['instance_params']['ALPHA_OP']*\
+				(instance['all_requests'][selected_request_as_list[0]]['our_cut_from_requester'][1] - \
+					(1 - instance['instance_params']['OUR_CUT_FROM_DRIVER']))*\
+				euclidean(instance['all_requests'][selected_request_as_list[0]]['orig'],instance['all_requests'][selected_request_as_list[0]]['dest'])
+
+def get_profit_matched(selected_requests,instance,permutation,experiment_params):
+	
+	assert len(selected_requests) == 2
+
+	instance_params = instance['instance_params']
+	all_requests = instance['all_requests']
+
+
+	if experiment_params['DISCOUNT_SETTING']=='independent':
+
+		return instance_params['ALPHA_OP']*\
+				(sum([all_requests[m]['our_cut_from_requester'][2]*\
+					euclidean(all_requests[m]['orig'],all_requests[m]['dest']) for m in selected_requests]) - \
+				(1-instance_params['OUR_CUT_FROM_DRIVER'])*get_driving_distance(None,selected_requests,permutation,instance))
+	else:
+
+		result = 0
+		for m in selected_requests:
+			beta2 = all_requests[m]['our_cut_from_requester'][1]
+			beta2 *= (1 - (get_driving_distance(m,selected_requests,permutation,instance) - \
+				euclidean(all_requests[m]['orig'],all_requests[m]['dest']))*\
+				1.0/euclidean(all_requests[m]['orig'],all_requests[m]['dest']))
+
+			result +=beta2*euclidean(all_requests[m]['orig'],all_requests[m]['dest'])
+
+		return instance_params['ALPHA_OP']*result - \
+			(1-instance_params['OUR_CUT_FROM_DRIVER'])*get_driving_distance(None,selected_requests,permutation,instance)
+
+
+def get_incremental_profit_subroutine(selected_requests,instance,experiment_params):
 	assert instance is not None
 	assert selected_requests is not None
 
 	#Local copies
 	all_requests = instance['all_requests']
-	params = instance['params']
+	instance_params = instance['instance_params']
+	all_permutations_two = instance['instance_params']['all_permutations_two']
 
-	if params['EXP_DISCOUNT_SETTING'] == 'detour_based':
+
+	if experiment_params['DISCOUNT_SETTING'] == 'detour_based':
 		check_SIR_routine = check_SIR_satisfaction_detour_based
-	elif params['EXP_DISCOUNT_SETTING'] == 'independent':
+	elif experiment_params['DISCOUNT_SETTING'] == 'independent':
 		check_SIR_routine = check_SIR_satisfaction_general
 
 	#Logic
@@ -217,45 +319,35 @@ def get_profit(selected_requests,instance):
 		return NotImplementedError
 
 	if len(selected_requests) == 1:
-		return [all_requests[selected_requests[0]]['our_cut_from_requester'][1]*euclidean(all_requests[selected_requests[0]]['orig'],all_requests[selected_requests[0]]['dest']),('si','di')]
+		return [ get_profit_unmatched(selected_requests,instance) , ('si','di') ]
 
 	if len(selected_requests) == 2:
 		i = selected_requests[0]
 		j = selected_requests[1]
 
-		all_permutations = {('si','sj','di','dj'):'case1',
-			('si','sj','dj','di'):'case2',
-			('sj','si','dj','di'):'case1',
-			('sj','si','di','dj'):'case2'}#TODO: make more comprehensible
-
-		profits = np.zeros(len(all_permutations))
+		profits = np.zeros(len(all_permutations_two))
 		max_profit = 0
 		max_profit_permutation = None
-		for k,permutation in enumerate(all_permutations):
-			SIR_satisfied = check_SIR_routine([i,j],permutation,instance, all_permutations)
+		for k,permutation in enumerate(all_permutations_two):
+			SIR_satisfied = check_SIR_routine([i,j],permutation,instance, experiment_params)
 			if SIR_satisfied is True:
-				profits[k] = params['ALPHA_OP']*(sum([all_requests[m]['our_cut_from_requester'][2]*euclidean(all_requests[m]['orig'],all_requests[m]['dest']) for m in (i,j)]) - (1-params['OUR_CUT_FROM_DRIVER'])*get_driving_distance([i,j],permutation,all_permutations,instance))
+				profits[k] = get_profit_matched([i,j],instance,permutation,experiment_params)
 			if profits[k] > max_profit:
 				max_profit_permutation = copy.deepcopy(permutation)
 				max_profit = profits[k]
 
 		return [max_profit,max_profit_permutation]
 
-def get_incremental_profit(selected_requests,instance):
+def get_incremental_profit(selected_requests,instance,experiment_params):
 	'''
 	if incremental profit is less than or equal to zero then there is
 	no way to match the selected requests in a way that satisfies SIR and is incremetally profitable
 	'''
 
-	result,result_permutation = get_profit(selected_requests,instance)
+	result,result_permutation = get_incremental_profit_subroutine(selected_requests,instance,experiment_params)
 	for i in selected_requests:
-		result -= get_profit([i],instance)[0] #TODO
-	#TODO: Call get_profit()
+		result -= get_profit_unmatched([i],instance)
 	return [result,result_permutation]
-
-
-#######################################################
-#Two request case
 
 def get_experiment_params():
 
@@ -266,7 +358,6 @@ def get_experiment_params():
 	return {'DISCOUNT_SETTING':DISCOUNT_SETTING,
 	'SIR_SETTING':SIR_SETTING,
 	'GAMMA':GAMMA}
-
 
 def is_ridesharing(i,instance):
 	if instance['all_requests'][i]['PROVIDER_MARKET']==True:
@@ -279,50 +370,105 @@ def match_requests(instance,experiment_params):
 	H = nx.Graph()
 	request_pairs_with_permutations = {}
 	non_ridesharing_requests = []
+	unmatched_requests_initial = set()
 	for i in instance['all_requests']:
 		if is_ridesharing(i,instance) is False:
+			# print "{0} is not ridesharing.".format(i)
 			non_ridesharing_requests.append(i)
 			continue
 		for j in instance['all_requests']:
-			if i != j and is_ridesharing(j,instance) is True:
-				[incremental_profit,optimal_permutation] = get_incremental_profit([i,j],instance,experiment_params)	#TODO CHANGE FUNCTION
+			if i < j and is_ridesharing(j,instance) is True:
+				unmatched_requests_initial.add(i)
+				unmatched_requests_initial.add(j)
+				#print "i,j = {0},{1}".format(i,j)
+				[incremental_profit,optimal_permutation] = get_incremental_profit([i,j],instance,experiment_params)
 				if  incremental_profit > 0:
 					H.add_edge(str(i), str(j), weight=incremental_profit)
 					request_pairs_with_permutations[(i,j)] = optimal_permutation
-					print "Include edge {0},{1}  :   {2}".format(i,j,incremental_profit)
+					# print "Include edge {0},{1}  :   {2}".format(i,j,incremental_profit)
 
+	#these requests failed to satisfy incremental profit > 0 for any of their potential ridesharing neighbors
+	#pprint(unmatched_requests_initial)
+	print H.nodes()
+	unmatched_requests_initial = [x for x in unmatched_requests_initial if str(x) not in H.nodes()]
+	#pprint(unmatched_requests_initial)
 
 	mates = nx.max_weight_matching(H)
 	matched_request_pairs_with_permutations = {}
 	for i,j in mates.items():
-		if i < j:
-			matched_request_pairs_with_permutations[(i,j)] = request_pairs_with_permutations[(i,j)]
-	unmatched_requests = [x in H.nodes() if x not in mates.keys()]
+		if int(i) < int(j):
+			# print "types: {0},{1},{2},{3}".format(i,type(i),j,type(j))
+			matched_request_pairs_with_permutations[(int(i),int(j))] = request_pairs_with_permutations[(int(i),int(j))]
+	unmatched_requests = [int(x) for x in H.nodes() if x not in mates.keys()]
+	#pprint(unmatched_requests)
+	if len(unmatched_requests_initial)>0:
+		if len(unmatched_requests) >0:
+			unmatched_requests.extend(unmatched_requests_initial)
+		else:
+			unmatched_requests = unmatched_requests_initial
 
-	return {'non_ridesharing_requests':non_ridesharing_requests,
+	result =  {'non_ridesharing_requests':non_ridesharing_requests,
 	'unmatched_requests':unmatched_requests,
 	'matched_request_pairs_with_permutations':matched_request_pairs_with_permutations}
 
+	# pprint(result)
 
-def get_profit_from_unmatched_requests(requests,instance):
-	return NotImplementedError
+	return result
 
-def get_profit_from_non_ridesharing_requests(requests,instance):
-	return NotImplementedError
+def get_profit_from_unmatched_requests(selected_requests,instance):
 
-def get_profit_from_matched_requests(matched_request_pairs_with_permutations,instance):
-	return NotImplementedError
+	if selected_requests is None:
+		return 0
+	elif len(selected_requests) == 0:
+		return 0
 
-if '__init__'==__main__:
+	result = 0
+	for i in selected_requests:
+		result += get_profit_unmatched([i],instance)
+
+	return result
+
+def get_profit_from_non_ridesharing_requests(selected_requests,instance):
+
+	if selected_requests is None:
+		return 0
+	elif len(selected_requests) == 0:
+		return 0
+
+
+	#print 'selected_requests',selected_requests
+
+	result = 0
+	for i in selected_requests:
+		result += get_profit_non_ride_sharing([i],instance)
+
+	return result
+
+def get_profit_from_matched_requests(matched_request_pairs_with_permutations,instance,experiment_params):
+
+	if len(matched_request_pairs_with_permutations) == 0:
+		return 0
+
+	result = 0
+	for request_pairs in matched_request_pairs_with_permutations:
+		result += get_profit_matched(request_pairs,instance,matched_request_pairs_with_permutations[request_pairs],experiment_params)
+	return result
+
+if __name__=='__main__':
 
 	instance = generate_instance()
 	experiment_params = get_experiment_params()
-
-	[non_ridesharing_requests,unmatched_requests,matched_request_pairs_with_permutations] = match_requests(instance,experiment_params)
-
+	solution = match_requests(instance,experiment_params)
 	total_profit = \
-		get_profit_from_non_ridesharing_requests(non_ridesharing_requests,instance) + \
-		get_profit_from_unmatched_requests(unmatched_requests,instance) + \
-		get_profit_from_matched_requests(matched_request_pairs_with_permutations,instance)
+		get_profit_from_non_ridesharing_requests(solution['non_ridesharing_requests'],instance) + \
+		get_profit_from_unmatched_requests(solution['unmatched_requests'],instance) + \
+		get_profit_from_matched_requests(solution['matched_request_pairs_with_permutations'],instance,experiment_params)
 
-	print total_profit
+	print "total profit:",total_profit
+	pprint(solution)
+
+	## debug
+	# for i in instance['all_requests']:
+	# 	if instance['all_requests'][i]['PROVIDER_MARKET']==True:
+	# 		if instance['all_requests'][i]['RIDE_SHARING']==True:
+	# 			print i
